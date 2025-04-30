302744a82153f5cdfb8d88945269892d8d2960f7
Vaibhav-Shinde <vbs.codestuff@gmail.com>
2025-04-30 16:51:17 +0530

    added common function

---
+++ src/datascience_project/utils/common.py
@@ -0,0 +1,66 @@
+import os
+import yaml
+from src.datascience_project import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+from box.exceptions import BoxValueError
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """
+    read yaml files and returs
+    Args:
+        path_to_yaml: path like input
+    Raises:
+        ValueError: if yaml file is empy
+        e: empty file
+    Returns:
+        ConfiBox: ConfigBox type
+    """
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directory(path_to_directory: list, verbose=True):
+    for path in path_to_directory:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved as: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file load from {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"Binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"Binary file loaded from {path}")
+    return data
